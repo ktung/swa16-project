15ab9c91aa2be0565e861e3b09376d342e037fce
Mathieu Molinengo <molinengo.mathieu@etu.unice.fr>
2016-10-25 14:07:32 +0200

    Site de base page d'accueil avec CSS plus ou moins correct à modifier au besoin

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HOTEL</title>
+    <meta charset="UTF-8">
+   <link rel="stylesheet" href="/assets/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="/assets/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/assets/assets/jquery-2.2.1.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+</head>
+<body>
+<table class="mainTab">
+    <tr class="logo" >        <td class="cote">&nbsp;</td>
+
+        <th class="logo" style="text-align: center" scope="col"><img src="pictures/Logo.png" width="504px" height="207px"  alt="Logo"/></th>        <td class="cote">&nbsp;</td>
+
+    </tr>
+    <tr class="menu">
+        <td class="cote">&nbsp;</td>
+        <td style="text-align: center">
+            <nav class="nav"><a class="btn btn-danger" type="button" href="">Home</a><a class="btn btn-danger" type="button" onclick=''>Sell</a><a 	class="btn btn-danger" type="button" onclick=''>Un autre Onglet</a><a class="btn btn-danger" type="button" onclick=''>Cart</a></nav></td>
+        <td class="cote">&nbsp;</td>
+    </tr>
+    <tr>        <td class="cote">&nbsp;</td>
+        <td class="cote">&nbsp;</td>
+        <td class="cote">&nbsp;</td>
+</tr>
+    <tr class="mainPart">
+        <td class="cote">&nbsp;</td>
+        <td id="cadrePrincipal"><!-- un include ça peut être chouette -->
+            <h1>Bienvenue dans notre super Hotel</h1><br/>
+            <p>Nous vous proposons un hotel atypique</p>
+            <br/>
+        </td>
+        <td class="cote">&nbsp;</td>
+    </tr>
+</table>
+</body>
+</html>
