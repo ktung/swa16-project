7a52707132dae92f2732b6572be4186686abdbe0
Lucas Martinez <lucasmartinez.it@gmail.com>
2016-11-06 14:04:11 +0100

    t'en as oublié

--- includes.inc.php
+++ includes.inc.php
@@ -1,9 +1,9 @@
-<link rel="stylesheet" href="/assets/styles.css" rel="stylesheet" />
+<link rel="stylesheet" href="assets/styles.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link href="/assets/css/bootstrap.css" rel="stylesheet">
-<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-<script src="/assets/jquery-2.2.1.js"></script>
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<script src="assets/jquery-2.2.1.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-<script src="/assets/js/vulneChambres.js"></script>
+<script src="assets/js/vulneChambres.js"></script>
