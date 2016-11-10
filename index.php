c253d567d738b963c805f42ab46d72ee6fa60812
Cindy Najjar <najjar@polytech.unice.fr>
2016-11-10 23:57:07 +0100

    faille A9 terminée

--- index.php
+++ index.php
@@ -5,18 +5,32 @@
     <meta charset="UTF-8">
     <?php include('includes.inc.php'); ?>
 </head>
+    <script
+    src="https://code.jquery.com/jquery-1.6.3.js"
+    integrity="sha256-m6oQ4cVjDD3Nm7Rr8AkTzJSzhV1YyUWa6YSDOcVm6Xs="
+    crossorigin="anonymous"></script>
 <body>
 <table class="mainTab">
     <?php include('menu.inc.php'); ?>
     <tr class="mainPart">
         <td class="cote">&nbsp;</td>
         <td id="cadrePrincipal"><!-- un include ça peut être chouette -->
-            <h1>Bienvenue dans notre super Hotel</h1><br/>
-            <p>Nous vous proposons un hotel atypique</p>
+            <h1 id="bienvenue">Bienvenue dans notre super Hotel</h1><br/>
+            <p>*** Nous vous proposons un hotel atypique ! ***</p>
             <br/>
         </td>
         <td class="cote">&nbsp;</td>
     </tr>
 </table>
 </body>
+
+<script type="text/javascript">
+if (window.location.href.indexOf("pseudo=")) {
+    var pseudo_position = location.href.indexOf("pseudo=");
+    var pseudo = location.href.substring(pseudo_position+7);
+    $("#bienvenue").html("Bienvenue, " + pseudo + "!\n");
+    $("#cadrePrincipal").append('<a target="_blank" href="http://secuweb.neoskai.com/chambres.php">Cliquez ici pour continuer</a>');
+}
+</script>
+
 </html>
