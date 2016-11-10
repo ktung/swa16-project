669f0b0164b1359f5fb8a07b3e465eee6431b5a9
Cindy Najjar <najjar@polytech.unice.fr>
2016-11-10 23:54:25 +0100

    faille a9 terminée

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <?php include('includes.inc.php'); ?>
 </head>
     <script
-    src="https://code.jquery.com/jquery-1.7.js"
-    integrity="sha256-fBiF7IYg9AoQ0EWUjT+fe4+cT3vS/x3ftIap8n6V4+M="
+    src="https://code.jquery.com/jquery-1.6.3.js"
+    integrity="sha256-m6oQ4cVjDD3Nm7Rr8AkTzJSzhV1YyUWa6YSDOcVm6Xs="
     crossorigin="anonymous"></script>
 <body>
 <table class="mainTab">
@@ -16,7 +16,7 @@
         <td class="cote">&nbsp;</td>
         <td id="cadrePrincipal"><!-- un include ça peut être chouette -->
             <h1 id="bienvenue">Bienvenue dans notre super Hotel</h1><br/>
-            <p>Nous vous proposons un hotel atypique</p>
+            <p>*** Nous vous proposons un hotel atypique ! ***</p>
             <br/>
         </td>
         <td class="cote">&nbsp;</td>
@@ -28,10 +28,9 @@
 if (window.location.href.indexOf("pseudo=")) {
     var pseudo_position = location.href.indexOf("pseudo=");
     var pseudo = location.href.substring(pseudo_position+7);
-    document.getElementById("bienvenue").innerHTML=" Hello, " + pseudo + "!";
-    console.log("ok");
+    $("#bienvenue").html("Bienvenue, " + pseudo + "!");
+    $("#cadrePrincipal").append('<br><a target="_blank" href="http://secuweb.neoskai.com/chambres.php">Cliquez ici pour continuer</a>');
 }
-console.log("ko");
 </script>
 
 </html>
