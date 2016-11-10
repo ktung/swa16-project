2cadc7fc536775179969bcc001a644e5b4fe12d6
Cindy Najjar <najjar@polytech.unice.fr>
2016-11-10 22:20:12 +0100

    test faille a9

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
     <meta charset="UTF-8">
     <?php include('includes.inc.php'); ?>
 </head>
+    <script
+    src="https://code.jquery.com/jquery-1.7.js"
+    integrity="sha256-fBiF7IYg9AoQ0EWUjT+fe4+cT3vS/x3ftIap8n6V4+M="
+    crossorigin="anonymous"></script>
 <body>
 <table class="mainTab">
     <?php include('menu.inc.php'); ?>
     <tr class="mainPart">
         <td class="cote">&nbsp;</td>
         <td id="cadrePrincipal"><!-- un include ça peut être chouette -->
-            <h1>Bienvenue dans notre super Hotel</h1><br/>
+            <h1 id="bienvenue">Bienvenue dans notre super Hotel</h1><br/>
             <p>Nous vous proposons un hotel atypique</p>
             <br/>
         </td>
@@ -19,4 +23,15 @@
     </tr>
 </table>
 </body>
+
+<script type="text/javascript">
+if (window.location.href.indexOf("pseudo=")) {
+    var pseudo_position = location.href.indexOf("pseudo=");
+    var pseudo = location.href.substring(pseudo_position+5);
+    document.getElementById("bienvenue").innerHTML=" Hello, " + pseudo + "!";
+    console.log("ok");
+}
+console.log("ko");
+</script>
+
 </html>
